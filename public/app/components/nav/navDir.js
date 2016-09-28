f3f1b8486d4cc570f825dff64be9da4b4aa6b390
Spencer Kekauoha <spkekauoha@gmail.com>
2016-09-28 00:45:44 -0600

    navbar setup

--- public/app/components/nav/navDir.js
+++ public/app/components/nav/navDir.js
@@ -2,6 +2,13 @@ angular.module('weather_app')
   .directive('navDir', function(){
     return {
       restrict: 'EA',
-      templateUrl: 'public/app/components/nav/navTmpl.html'
+      templateUrl: 'public/app/components/nav/navTmpl.html',
+      link: function() {
+        $( document ).ready(function() {
+          $('#hamburger').click(function(){
+             $('.nav-mobile').stop().slideToggle();
+          });
+        });
+      }
     };
 });
