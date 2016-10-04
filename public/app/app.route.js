15a72388c59682e40f776d8e6832258a34f7e14d
Spencer Kekauoha <spkekauoha@gmail.com>
2016-10-04 10:08:36 -0600

    removed search route and template. minified route cdn

--- public/app/app.route.js
+++ public/app/app.route.js
@@ -6,10 +6,6 @@ angular.module('weather_app', ["ui.router"])
         url: '/landing',
         templateUrl: 'public/app/components/landing/landingTmpl.html'
       })
-      .state('search', {
-        url: '/search',
-        templateUrl: 'public/app/components/search/searchTmpl.html'
-      })
       .state('dashboard', {
         url: '/dashboard',
         templateUrl: 'public/app/components/dashboard/dashboardTmpl.html',
